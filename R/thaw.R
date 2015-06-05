366510339b99061fe84db1a7da87774588183d87
Mullefa <gccdawson@gmail.com>
2015-06-05 16:14:32 +0100

    stricter checks with thaw(); and packages will not be installed if they are already in the virtual environment

--- R/thaw.R
+++ R/thaw.R
@@ -22,14 +22,36 @@ thaw <- function(...) {
 
   for (pkg in pkgs$packages) {
     if (pkg$name %in% loadedNamespaces()) {
-      warning(
+      stop(
         "the namespace of ", pkg$name, " is currently loaded. ",
         "Not attempting installation into the local library.",
         call. = FALSE
       )
-    } else {
-      install.packages(pkg$name, ...)
     }
   }
+
+  lib_path <- file.path(meta_data$path, "library")
+  lib_pkgs <- read_dcfs(lib_path)
+
+  for (pkg in pkgs$packages) {
+    lib_pkg <- lib_pkgs[[pkg$name]]
+
+    if (!is.null(lib_pkg) && pkg$version == lib_pkg$Version) {
+      message(pkg$name, " ", bracket(pkg$version)," already installed locally")
+    }  else {
+      download_and_install(pkg$name, pkg$version)
+    }
+  }
+}
+
+
+download_and_install <- function(pkg, version) {
+  url <- pkg_url(pkg, version)
+  pkg <- pkg_file(pkg, version)
+
+  download.file(url, pkg)
+  install.packages(pkg, repos = NULL, type = "source")
+
+  unlink(pkg)
 }
 
